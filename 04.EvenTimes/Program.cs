587504bcd8c184cbba5706cc747c7ebda66797ac
AnitoKarakasheva <134801293+AnitoKarakasheva@users.noreply.github.com>
2024-05-22 18:05:21 +0300

    Add project files.

---
+++ 04.EvenTimes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(inputNumber))
+                {
+                    numbers.Add(inputNumber, 0);
+                }
+
+                numbers[inputNumber]++;
+            }
+
+            int evenNumber = numbers
+                .Where(x => x.Value % 2 == 0)
+                .FirstOrDefault()
+                .Key;
+
+            //int evenNumber = 0;
+
+            //foreach (var (key, value) in numbers)
+            //{
+            //    if (value % 2 == 0)
+            //    {
+            //        evenNumber = key;
+            //        break;
+            //    }
+            //}
+
+            Console.WriteLine(evenNumber);
+        }
+    }
+}
+      
---
+++ 05.CountSymbols/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            SortedDictionary<char, int> charOccurences = new SortedDictionary<char, int>();
+
+            foreach (var currentChar in input)
+            {
+                if (!charOccurences.ContainsKey(currentChar))
+                {
+                    charOccurences.Add(currentChar, 0);
+                }
+
+                charOccurences[currentChar]++;
+            }
+
+            foreach (var (symbol, occurences) in charOccurences)
+            {
+                Console.WriteLine($"{symbol}: {occurences} time/s");
+            }
+        }
+    }
+}
+       
